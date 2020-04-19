ef6e3b306720dd13b09f74f1f5840990e9c663eb
shuzonudas <shuzonudas@yahoo.com>
2020-04-20 09:21:39 +0930

    Create SelectionList.ino

---
+++ U8g2/Examples/full_buffer/SelectionList/SelectionList.ino
@@ -0,0 +1,93 @@
+/*
+  Universal 8bit Graphics Library (https://github.com/olikraus/u8g2/)
+
+  Copyright (c) 2016, olikraus@gmail.com
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, 
+  are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice, this list 
+    of conditions and the following disclaimer.
+    
+  * Redistributions in binary form must reproduce the above copyright notice, this 
+    list of conditions and the following disclaimer in the documentation and/or other 
+    materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND 
+  CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, 
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF 
+  MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE 
+  DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, 
+  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT 
+  NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; 
+  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER 
+  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, 
+  STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF 
+  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.  
+*/
+
+#include <Arduino.h>
+#include <U8g2lib.h>
+
+monoView_u8g2 u8g2(U8G2_R0);
+//u8g2.setBacklight(225);
+
+
+void setup(void) {
+  
+  // DOGS102 Shield (http://shieldlist.org/controlconnection/dogs102)
+  // u8g2.begin(/* menu_select_pin= */ 5, /* menu_next_pin= */ 4, /* menu_prev_pin= */ 2, /* menu_home_pin= */ 3);
+  
+  // DOGM128 Shield (http://shieldlist.org/schmelle2/dogm128) + DOGXL160 Shield
+  // u8g2.begin(/* menu_select_pin= */ 2, /* menu_next_pin= */ 3, /* menu_prev_pin= */ 7, /* menu_home_pin= */ 8);
+
+  // MKR Zero Test Board
+  //u8g2.begin(/*Select=*/ 0, /*Right/Next=*/ 1, /*Left/Prev=*/ 2, /*Up=*/ 4, /*Down=*/ 3, /*Home/Cancel=*/ A6); 
+
+  // Arduboy
+  //u8g2.begin(/*Select=*/ A0, /*Right/Next=*/ 5, /*Left/Prev=*/ 9, /*Up=*/ 8, /*Down=*/ 10, /*Home/Cancel=*/ A1); // Arduboy DevKit
+  //MonoView
+  u8g2.begin(/*Select=*/ 8, /*Right/Next=*/ 3, /*Left/Prev=*/ 5, /*Up=*/ 3, /*Down=*/ 5, /*Home/Cancel=*/ 11); 
+  u8g2.setBacklight(225);
+  u8g2.setFont(u8g2_font_6x12_tr);
+}
+
+const char *string_list = 
+  "Altocumulus\n"
+  "Altostratus\n"
+  "Cirrocumulus\n"
+  "Cirrostratus\n"
+  "Cirrus\n"
+  "Cumulonimbus\n"
+  "Cumulus\n"
+  "Nimbostratus\n"
+  "Stratocumulus\n"
+  "Stratus";
+
+uint8_t current_selection = 0;
+
+
+void loop(void) {
+
+  current_selection = u8g2.userInterfaceSelectionList(
+    "Cloud Types",
+    current_selection, 
+    string_list);
+
+  if ( current_selection == 0 ) {
+    u8g2.userInterfaceMessage(
+	"Nothing selected.", 
+	"",
+	"",
+	" ok ");
+  } else {
+    u8g2.userInterfaceMessage(
+	"Selection:", 
+	u8x8_GetStringLineStart(current_selection-1, string_list ),
+	"",
+	" ok \n cancel ");
+  }
+}
